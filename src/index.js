2d3e5e3fa3067c05260444261cf3ea987be43d60
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-12-22 10:33:44 +0100

    :wrench: Setting up Redux Thunk

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 //Redux Setup
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReduceer from "./reducers";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReduceer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
